ce63b97c755297e4d1688026489dc95a41e5278b
Aninhasanchez <analuisasanchez05@gmail.com>
2025-08-25 14:18:54 -0300

    25-08

--- kanban/src/Paginas/CadUsuario.jsx
+++ kanban/src/Paginas/CadUsuario.jsx
@@ -2,17 +2,38 @@ import { useForm } from "react-hook-form";
 import {z} from 'zod';
 import {zodResolver} from "@hookform/resolvers/zod";
 
-const schemaCadUsuario = 
+const schemaCadUsuario = z.object({
+    nome: z.string()
+    .min(1, 'Informe seu nome, com pelo menos 1 caractere')
+    .max(50, 'Informe no máximo ')
+})
 
 export function CadUsuario(){
+    const{
+        register,
+        handleSubmit,
+        formState: {errors}, reset
+    } = useForm({
+        resolver:zodResolver(schemaCadUsuario)
+    });
+
+    async function obterDados(data) {
+        console.log
+        
+    }
+
+
+
     return(
         <form>
             <h2>Cadastro de Usuáeio</h2>
             <label>Nome:</label>
-            <input type="text" placeholder="José da Silva"></input>
+            <input type="text" placeholder="José da Silva" required {...register("nome")}></input>
+            {errors.nome&& <p>{errors.nome.message}</p>}
 
             <label>Email: </label>
-            <input type="email" placeholder="email@email.com"></input>
+            <input type="email" placeholder="email@email.com" ></input>
+            {errors.email&& <p>{errors.email.message}</p>}
 
             <button type="submit">Cadastrar</button>
 
